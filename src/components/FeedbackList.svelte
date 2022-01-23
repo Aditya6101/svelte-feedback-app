a12bd7b6d9b0f5a89b6e161d8517a80b3aa4b454
Aditya Kamble <adityakamble6101@gmail.com>
2022-01-23 17:19:05 +0530

    transitions added.

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,11 +1,14 @@
 <script>
+  import { fade, scale } from 'svelte/transition';
   import FeebackItem from './FeedbackItem.svelte';
 
   export let feedback = [];
 </script>
 
 {#each feedback as fb (fb.id)}
-  <FeebackItem item={fb} on:delete-fb />
+  <div in:scale={{ duration: 500 }} out:fade={{ duration: 500 }}>
+    <FeebackItem item={fb} on:delete-fb />
+  </div>
 {/each}
 
 <style></style>
