28a816da5346a8672e9314e3245b3d4b1fe02054
Aditya Kamble <adityakamble6101@gmail.com>
2022-01-23 18:10:31 +0530

    store added.

---
+++ src/stores.js
@@ -0,0 +1,21 @@
+import { writable } from 'svelte/store';
+
+const initialFeedbacks = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+];
+
+export const FeedbackStore = writable(initialFeedbacks);
